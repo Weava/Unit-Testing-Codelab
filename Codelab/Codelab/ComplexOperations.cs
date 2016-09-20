f95364e15bbd8285e97152c645eaa68a8f68e3be
Aaron Weaver <aaron@appweava.com>
2016-09-19 18:45:50 -0700

    Added operations functions

---
+++ Codelab/Codelab/ComplexOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codelab
+{
+    class ComplexOperations
+    {
+        public double squareRoot(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
---
+++ Codelab/Codelab/SimpleOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codelab
+{
+    class SimpleOperations
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
