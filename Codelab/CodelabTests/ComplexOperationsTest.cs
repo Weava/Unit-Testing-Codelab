7b6bba59530fda299658410d83b656b7a7d1591d
Aaron Weaver <aaron@appweava.com>
2016-09-20 17:40:22 -0700

    Finalized tests for csharp

---
+++ Codelab/CodelabTests/ComplexOperationsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodelabTests
+{
+    [TestClass]
+    public class ComplexOperationsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
